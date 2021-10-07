483bf15d750946968b755d94a562b20ca58e05cb
Ichsanul Dwi Prayitno <58975208+DwiBlackList@users.noreply.github.com>
2021-10-07 23:16:38 +0700

    Create index.php

---
+++ Pemograman Web Dinamis/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <center><h1 style="font-family:cursive; color:lightgreen">Mari Membaca</h1></center>
+    </header>
+
+    <section>
+        <nav>
+            <ul>
+                <li><a href="#">Katalog</a></li>
+                <li><a href="#">Data</a></li>
+                <li><a href="#">Setting</a></li>
+            </ul>
+        </nav>
+
+        <article>
+            <h1 style="font-family:cursive; color:lightcoral">Teman baca kalian ..</h1>
+            <p style="font-family: cursive; ">
+                <q>Buku itu seperti cermin : kalau yang berkaca padanya adalah seorang yang bodoh , engkau tak bisa berharap yang terpantul adalah orang yang jenius</q>
+                <b>J.K Rowling</b>
+            </p>
+        </article>
+
+        <article>
+            <h1 style="font-family: cursive; color: lightcoral;">Baca quote ini ya ..</h1>
+            <img src="../../img/buku1.jpg" alt="" width="150px" height="150">
+            <img src="../../img/buku2.jpg" alt="" width="150px" height="150">
+            <img src="../../img/buku3.jpg" alt="" width="150px" height="150">
+        </article>
+
+        <footer>
+            <p>Polines 2020</p>
+        </footer>
+    </section>
+</body>
+</html>
