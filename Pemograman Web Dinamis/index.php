0a5692c467f064674dcb21a351c0e310cf63b80b
Ichsanul Dwi Prayitno <58975208+DwiBlackList@users.noreply.github.com>
2021-10-07 23:23:07 +0700

    Add files via upload

---
+++ Pemograman Web Dinamis/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Ichsanul Dwi Prayitno</title>
+
+    <style>
+        body{
+            color : white;
+            background-color: black;
+            align : center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Kumpulan Tugas Jobsheet</h1><br>
+        <p>By : Ichsanul Dwi Prayitno</p><br>
+        <p>Teknologi Rekayasa Komputer</p><br>
+        <p>TI-2A 4.33.20.0.14</p>
+        <marquee behavior="" direction="">Entahlah ~</marquee>
+    </div>
+    <div class="container mt-3">
+        <ul class="nav flex-column">
+            <!-- <li class="nav-item">
+                <button type="button" class="btn btn-secondary"><a class="nav-link text-white" href="index.php">Home</a></button>
+            </li> -->
+            <li class="nav-item">
+            <div class="dropdown">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
+                Jobsheet 2 : Tag Dasar
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="Jobsheet2/hello_world.html">Hello World</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/tag.html">Tag</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/attribute.html">Attribute</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/tag_header.html">Tag Header</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/style.html">Style</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/format.html">Format HTML</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/color.html">HTML Color</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/link.html">Link</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/list.html">List</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/table.html">Table</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/sematic.html">Sematics</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/form.html">Form</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/proyek">Proyek</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/biodata.html">Biodata</a></li>
+                    <li><a class="dropdown-item" href="Jobsheet2/php.html">PHP</a></li>
+                </ul>
+            </div>
+            </li>
+            <li class="nav-item">
+                <div class="dropdown">
+                    <button type="button" class="btn btn-primary dropdown-toggle disabled" data-bs-toggle="dropdown">
+                    Under Construction
+                    </button>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="dropdown">
+                    <button type="button" class="btn btn-primary dropdown-toggle disabled" data-bs-toggle="dropdown">
+                    Under Construction
+                    </button>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+</body>
+</html>
