59aefd27f32e05dcb9895ad97d6a72a143f746f4
Ichsanul Dwi Prayitno <58975208+DwiBlackList@users.noreply.github.com>
2021-11-07 15:47:23 +0700

    Update index.php

--- Pemograman Web Dinamis/index.php
+++ Pemograman Web Dinamis/index.php
@@ -13,6 +13,9 @@
             background-color: black;
             align : center;
         }
+        #margin{
+            margin: 5px;
+        }
     </style>
 </head>
 <body>
@@ -29,41 +32,109 @@
                 <button type="button" class="btn btn-secondary"><a class="nav-link text-white" href="index.php">Home</a></button>
             </li> -->
             <li class="nav-item">
-            <div class="dropdown">
-                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-                Jobsheet 2 : Tag Dasar
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="Jobsheet2/hello_world.html">Hello World</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/tag.html">Tag</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/attribute.html">Attribute</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/tag_header.html">Tag Header</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/style.html">Style</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/format.html">Format HTML</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/color.html">HTML Color</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/link.html">Link</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/list.html">List</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/table.html">Table</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/sematic.html">Sematics</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/form.html">Form</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/proyek">Proyek</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/biodata.html">Biodata</a></li>
-                    <li><a class="dropdown-item" href="Jobsheet2/php.html">PHP</a></li>
-                </ul>
-            </div>
-            </li>
-            <li class="nav-item">
                 <div class="dropdown">
-                    <button type="button" class="btn btn-primary dropdown-toggle disabled" data-bs-toggle="dropdown">
-                    Under Construction
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" id="margin">
+                    Jobsheet 2 : Tag Dasar
                     </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="Jobsheet2/hello_world.html" target="_blank">Hello World</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/tag.html" target="_blank">Tag</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/attribute.html" target="_blank">Attribute</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/tag_header.html" target="_blank">Tag Header</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/style.html" target="_blank">Style</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/format.html" target="_blank">Format HTML</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/color.html" target="_blank">HTML Color</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/link.html" target="_blank">Link</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/list.html" target="_blank">List</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/table.html" target="_blank">Table</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/sematic.html" target="_blank">Sematics</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/form.html" target="_blank">Form</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/proyek" target="_blank">Proyek</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/biodata.html" target="_blank">Biodata</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet2/php.html" target="_blank">PHP</a></li>
+                    </ul>
                 </div>
             </li>
             <li class="nav-item">
                 <div class="dropdown">
-                    <button type="button" class="btn btn-primary dropdown-toggle disabled" data-bs-toggle="dropdown">
-                    Under Construction
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" id="margin">
+                    Jobsheet 3 : Styling With CSS
                     </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="Jobsheet3/selector.html" target="_blank">Selector</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/external_style_sheet.html" target="_blank">External Style Sheet</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/color.html" target="_blank">CSS Colors</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/group_selector.html" target="_blank">Group Selector</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/rule_konflik.html" target="_blank">Rule Konflik</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/rule_konflik2.html" target="_blank">Rule Konflik 2</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/class_selector.html" target="_blank">Class Selector</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/attribute_selector.html" target="_blank">Attribute Selector</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/margin.html" target="_blank">Margin</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/padding.html" target="_blank">Padding</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/element_width.html" target="_blank">Element Width</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/element_model_box.html" target="_blank">Element Model Box</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/element_text.html" target="_blank">Element Text</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/element_list.html" target="_blank">Element List</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/table.html" target="_blank">Table</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/navigasi.html" target="_blank">Navigasi</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/multi_kolom.html" target="_blank">Multi Kolom</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet3/layout.html" target="_blank">Layout</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="dropdown">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" id="margin">
+                    Jobsheet 4 : PHP Dasar
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="Jobsheet4/hello_world.php" target="_blank">Hello World</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/case_sensitive.php" target="_blank">Case Sensitive</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/literal_string.php" target="_blank">Literal String</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/variable.php" target="_blank">Variable</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/tipe_data_variable.php" target="_blank">Tipe Data Variable</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/casting_variable.php" target="_blank">Casting Variable</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/konversi_nilai.php" target="_blank">Konversi Nilai</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/konversi_nilai2.php" target="_blank">Konversi Nilai 2</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/gabung_variable_string.php" target="_blank">Penggabungan Variable Dan String</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/penjumlahan_penggabungan_variable.php" target="_blank">Penjumlahan Variable</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/konstanta.php" target="_blank">Konstanta</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/string.php" target="_blank">String</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/operator_aritmatika.php" target="_blank">Operator Aritmatika</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/operator_penugasan.php" target="_blank">Operator Penugasan</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/operator_perbandingan.php" target="_blank">Operator Perbandingan</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/increment_decrement.php" target="_blank">Increment Decrement</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/operator_logika.php" target="_blank">Operator Logika</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet4/kalkulator.php" target="_blank">Tugas</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <div class="dropdown">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" id="margin">
+                    Jobsheet 5 : PHP Dasar
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="Jobsheet5/if.php" target="_blank">Kondisi If</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/if_else.php" target="_blank">Kondisi If Else</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/if_else2.php" target="_blank">Kondisi If Else 2</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/switch_case.php" target="_blank">Kondisi Switch Case</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/switch_case2.php" target="_blank">Kondisi Switch Case 2</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/operator_tertiary.php" target="_blank">Operator Tertiary</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/pernyataan_while.php" target="_blank">Pernyataan While</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/pernyataan_do_while.php" target="_blank">Pernyataan Do While</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/pernyataan_for.php" target="_blank">Pernyataan For</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/for_deret_genap.php" target="_blank">Deret Genap</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/for_deret_turun.php" target="_blank">Deret Turun</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/for_segitiga.php" target="_blank">For Segitiga *</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/break.php" target="_blank">Break</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/break2.php" target="_blank">Break 2</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/continue.php" target="_blank">Continue</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/continue2.php" target="_blank">Continue 2</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/exit.php" target="_blank">Exit</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/nolmenyatu.php" target="_blank">Null Coalesce Operator</a></li>
+                        <li><a class="dropdown-item" href="Jobsheet5/bilanganprima.php" target="_blank">Tugas : Bilangan Prima</a></li>
+                    </ul>
                 </div>
             </li>
         </ul>
