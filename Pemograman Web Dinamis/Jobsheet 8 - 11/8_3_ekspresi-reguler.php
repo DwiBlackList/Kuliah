efa0355a4c635a56ca4b151ee248c10d0c9cfa24
Ichsanul Dwi Prayitno <58975208+DwiBlackList@users.noreply.github.com>
2021-11-26 22:33:55 +0700

    Update 8_3_ekspresi-reguler.php
    memperbaiki error ekspresi

--- Pemograman Web Dinamis/Jobsheet 8 - 11/8_3_ekspresi-reguler.php
+++ Pemograman Web Dinamis/Jobsheet 8 - 11/8_3_ekspresi-reguler.php
@@ -7,23 +7,23 @@
     <title>Document</title>
 </head>
 <body>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>;">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         Email : <input type="text" name="email" id=""> <br> 
         <input type="submit" value="Cek">
     </form>
     <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST"){
-            $email = $_POST['email'];
-            if (empty($_POST['email'])) {
+            $email = $_POST["email"];
+            if (empty($_POST["email"])) {
                 echo "email harus diisi";
             } else {
-                $ekspresi = "/^[_a-z0-9-]+(\.[_a-z0-9+)*@[a-z0-9-]+(\.[_a-z0-9+)*(\.[a-z]{2,})$/i";
-            }
+                $ekspresi = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
 
-            if (preg_match($ekspresi, $email)){
-                echo "Alamat email <b>".$email."</b> valid";
-            } else {
-                echo "Alamat email <b>".$email."</b> tidak valid";
+                if (preg_match($ekspresi, $email)){
+                    echo "Alamat email <b>".$email."</b> valid";
+                }else{ 
+                    echo "Alamat email <b>".$email."</b> tidak valid";
+                }
             }
         }
     ?>
