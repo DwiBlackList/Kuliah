cba69617b8351cb778476a20d62b5e32df78e676
Ichsanul Dwi Prayitno <58975208+DwiBlackList@users.noreply.github.com>
2021-11-26 14:30:28 +0700

    Update 10_2_tampil-hari-tanggal.php
    memperbaiki error pada penggunaan kurung yang tidak sesuai

--- Pemograman Web Dinamis/Jobsheet 8 - 11/10_2_tampil-hari-tanggal.php
+++ Pemograman Web Dinamis/Jobsheet 8 - 11/10_2_tampil-hari-tanggal.php
@@ -12,13 +12,13 @@
         $nama_bulan = array("" , "Januari" , "Februari" , "Maret" , "April" , "Mei" , "Juni" , "Juli" , "Agustus" , "September" , "Oktober" , "November" , "Desember");
 
         $kode_hari = date("w");
-        $hari = $nama_hari($kode_hari);
+        $hari = $nama_hari[$kode_hari];
         $tanggal = date("j");
         $kode_bulan = date("m");
-        $bulan = $nama_bulan($kode_bulan);
+        $bulan = $nama_bulan[$kode_bulan];
         $tahun = date("Y");
 
-        echo "Sekarang hari".$hari.", tanggal ".$tanggal." ".$bulan." ".$tahun."<br>";
+        echo "Sekarang hari ".$hari.", tanggal ".$tanggal." ".$bulan." ".$tahun."<br>";
         print("Sekarang hari $hari, tanggal $tanggal $bulan $tahun ");
     ?>
 </body>
