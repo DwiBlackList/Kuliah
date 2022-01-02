c5a4a98d59bdf7a5db270cc0e3b9bf0c0aa899e2
Ichsanul Dwi Prayitno <58975208+DwiBlackList@users.noreply.github.com>
2022-01-02 19:05:35 +0700

    Update hapus_data_peminjam.php

--- Pemograman Web Dinamis/Jobsheet 15/hapus_data_peminjam.php
+++ Pemograman Web Dinamis/Jobsheet 15/hapus_data_peminjam.php
@@ -4,8 +4,8 @@ include "config.php";
 $db = new Database();
 if (isset($_GET['id'])) {
     $kode_peminjam = $_GET['id'];
-    $kode_peminjam = $db->kode_peminjam($kode_peminjam);
-    $kode_peminjam = $data_peminjam[0]['kode_peminjam'];
+//     $kode_peminjam = $db->kode_peminjam($kode_peminjam);
+//     $kode_peminjam = $data_peminjam[0]['kode_peminjam'];
 
     $db->hapus_data_peminjam($kode_peminjam);
     header("Location : tampilkan_data_peminjam.php");
